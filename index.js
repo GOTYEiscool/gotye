1c1f97772982cd9a63e98200609bf2daeffe56f9
sldfnsadcsdgwrg <126883193+sldfnsadcsdgwrg@users.noreply.github.com>
2023-03-03 13:42:28 +0000

    speen

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+var speed=10
 document.addEventListener("keypress", function (event){
     document.getElementById("gotye").classList.toggle("reverse");
+    speed=speed-0.1
+    document.getElementById("gotye").style.animationDuration=speed + "s"
 })
