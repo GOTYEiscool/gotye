7ced58ea06b10c1ba70b5479932ff69c191a3626
sldfnsadcsdgwrg <126883193+sldfnsadcsdgwrg@users.noreply.github.com>
2023-03-03 13:54:14 +0000

    hotfix again

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ document.addEventListener("keypress", function (event){
     speed=speed-0.1
     document.getElementById("gotye").style.animationDuration=speed + "s"
 })
+
+document.getElementById("button1").addEventListener("click", function() {
+    window.location.href="https://youtu.be/8UVNT4wvIGY"
+})
